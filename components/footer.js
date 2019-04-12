138ca3999cd18bba5c3507cf78e791364fbf5bac
Amo DelBello <amodelbello@gmail.com>
2019-04-12 16:58:53 -0600

    Adds eslint and prettier integration

--- components/footer.js
+++ components/footer.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React from 'react';
 
-export default () => {
-  return (
-    <footer>
-      <ul>
-        <li>item 1</li>
-        <li>item 2</li>
-        <li class="last">item 3</li>
-      </ul>
+const Footer = () => (
+  <footer>
+    <ul>
+      <li>item 1</li>
+      <li>item 2</li>
+      <li className="last">item 3</li>
+    </ul>
 
-      <style jsx>{`
-        footer ul li {
-          float: left;
-          margin-right: 60px;
-        }
-        footer ul li.last {
-          clear: both:
-        }
-      `}</style>
-    </footer>
-  );
-};
+    <style jsx>{`
+      footer ul li {
+        float: left;
+        margin-right: 60px;
+      }
+      footer ul li.last {
+        clear: both:
+      }
+    `}</style>
+  </footer>
+);
+
+export default Footer;
---
+++ pages/detail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Detail = () => (
+  <React.Fragment>
+    <h1>Name</h1>
+    <seciton>Hello</seciton>
+  </React.Fragment>
+);
+
+export default Detail;
