b369e6f5622f1e5e203a589156ca11192be4a46f
Amo DelBello <amodelbello@gmail.com>
2019-04-16 15:50:55 -0600

    Add test buttons to login page
    simple buttons to log in/out and output token to browser

--- pages/login.js
+++ pages/login.js
@@ -4,25 +4,46 @@ import PropTypes from 'prop-types';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import withData from '../lib/withData';
-
+import tokenHelper from '../lib/tokenHelper';
 import { Layout } from '../components/layout';
 
 class Login extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  state = {
+    token: 'hello',
+  };
 
   title = 'Login';
+
+  componentDidMount() {
+    const token = tokenHelper.getToken();
+    this.setState({
+      token,
+    });
+  }
 
   login = async () => {
     const result = await this.props.loginMutation({
       variables: {
-        email: '1testuser@lalala.com',
+        email: 'testuser@lalala.com',
         password: 'password1',
       },
     });
-    console.log('result', result);
+    const { login } = result.data;
+    tokenHelper.storeToken(login);
+
+    const token = tokenHelper.getToken();
+    this.setState({
+      token,
+    });
+    console.log('result', login);
     // Router.push('/');
+  };
+
+  logout = async () => {
+    tokenHelper.removeToken();
+    this.setState({
+      token: 'hello',
+    });
   };
 
   render() {
@@ -33,8 +54,12 @@ class Login extends React.Component {
           <button className="btn btn-dark btn-lg" onClick={this.login}>
             Login
           </button>
+          <button className="btn btn-dark btn-lg" onClick={this.logout}>
+            Logout
+          </button>
         </div>
 
+        <h1>Token: {this.state.token}</h1>
         <style jsx>{`
           .title {
             padding-top: 40px;
