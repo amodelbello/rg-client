28327cb90c416a8de532246bb29d91bb3af18114
Amo DelBello <amodelbello@gmail.com>
2019-04-15 22:38:12 -0600

    Add sticky footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,21 +1,10 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="container page-footer font-small blue pt-4">
-    <div className="container-fluid text-center text-md-left">
-      <div className="row">
-        <div className="col">
-          <h5 className="text-uppercase">Footer Content</h5>
-          <p>
-            Here you can use rows and columns here to organize your footer Here
-            you can use rows and columns here to organize your footer Here you
-            can use rows and columns here to organize your footer content.
-          </p>
-        </div>
-      </div>
+  <footer className="footer">
+    <div className="container text-center">
+      <span className="text-muted">&copy; {new Date().getFullYear()} ADB</span>
     </div>
-
-    <div className="footer-copyright text-center py-3">© 2019 Copyright</div>
   </footer>
 );
 
